4b7ba1d174f087e2a6785bff9507bc3cfc6dfa04
sahilsharma3942 <sahilsharma3942@gmail.com>
2024-10-20 23:44:32 +0530

    routing err

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { SidebarProvider } from "./context/sidebarContext";
-import { HashRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { SidebarProvider } from './context/sidebarContext';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <SidebarProvider>
-      <App />
-    </SidebarProvider>
-  </HashRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <SidebarProvider>
+    <App />
+  </SidebarProvider>
 );
